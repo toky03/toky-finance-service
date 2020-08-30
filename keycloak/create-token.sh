2726fa73831ea3bbcf81d2288888d87a8d476c8b
Marco <marco.jakob3@gmail.com>
2020-08-30 14:02:52 +0200

    keycloak config

--- keycloak/create-token.sh
+++ keycloak/create-token.sh
@@ -1,6 +1,10 @@
-export TKN=$(curl -X POST 'http://localhost:8080/auth/realms/master/protocol/openid-connect/token' \
+export TKN=$(curl -X POST 'http://localhost:8080/auth/realms/toky-finance/protocol/openid-connect/token' \
  -H "Content-Type: application/x-www-form-urlencoded" \
- -d "username=admin" \
+ -d "client_secret=46dd09e3-19f6-457d-8ec3-0ea0264cefde" \
+ -d "username=finance-service" \
  -d 'password=admin' \
  -d 'grant_type=password' \
- -d 'client_id=admin-cli' | jq -r '.access_token')
+  -d 'scope=openid' \
+ -d 'client_id=toky-finance-service' | jq -r '.access_token')
+
+#   -d "client_secret=46dd09e3-19f6-457d-8ec3-0ea0264cefde" \
